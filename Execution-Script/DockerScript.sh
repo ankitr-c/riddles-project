40315e5bc7071c00774c61927feccd95ccd8afbf
Ankit <ankit.raut@coditas.com>
2023-12-20 00:05:18 +0530

    initial commit

--- Execution-Script/DockerScript.sh
+++ Execution-Script/DockerScript.sh
@@ -2,12 +2,13 @@
 
 # Author: Ankit Raut 
 
-# Description: This Docker Script will install and configure the database.
+# Description: This Docker Script will install docker, dockercomopose and configure the Backend.
 
 setVariables()
 {
     #defining directory path.
     path="/home/ankitraut0987/vpc-project"
+
 }
 
 getVariables()
@@ -24,14 +25,18 @@ cd
 # sudo apt-get install docker -y >/dev/null && echo "** Successfully Installed Docker **" || { echo "Failed to Install Docker"; exit 1; }
 # sudo apt-get install docker-compose -y >/dev/null && echo "** Successfully Installed Docker-Compose **" || { echo "Failed to Install Docker-Copmose"; exit 1; }
 
+
 sudo apt-get install docker -y && echo "** Successfully Installed Docker **" || { echo "Failed to Install Docker"; exit 1; }
 sudo apt-get install docker-compose -y && echo "** Successfully Installed Docker-Compose **" || { echo "Failed to Install Docker-Copmose"; exit 1; }
-
 
 cd "$path"
 
 sudo docker-compose down && echo "Docker Is Down Now" || echo "Docker Already Down"
 
-sudo docker-compose up -d || echo "error in compose file"
+# sudo docker rmi staticip-multivm-dockerproject_backend || echo "Error deleting backend image"
+sudo docker rmi vpc-project_backend || echo "Error deleting backend image"
 
+
+
+sudo docker-compose up -d || echo "error in compose file"
 
