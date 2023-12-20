816b35a3d4d0e674ae5feff754adc79c13610d3e
Ankit <ankit.raut@coditas.com>
2023-12-21 00:27:12 +0530

    fixed jsonify

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,44 @@
+# from flask import Flask, jsonify, request
+# from pymongo import MongoClient
+# import random
+# from flask_cors import CORS
+
+# app = Flask(__name__)
+# CORS(app)
+# client = MongoClient("mongodb://root:root@localhost:27017")
+# db = client["riddles"]
+# riddles_collection = db["riddles_collection"]
+
+# @app.route('/get_riddle')
+# def get_riddle():
+#     pipeline = [{'$sample': {'size': 1}}]
+#     try:
+#         random_riddle = riddles_collection.aggregate(pipeline).next()
+#         return jsonify(random_riddle)
+#     except StopIteration:
+#         return jsonify({"msg": "No riddles available"})
+
+# @app.route('/add_riddle', methods=['POST'])
+# def add_riddle():
+#     try:
+#         new_riddle = request.json
+#         riddles_collection.insert_one(new_riddle)
+#         return jsonify({"msg": "Riddle added successfully"})
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
+
+# @app.route('/get_all_riddles')
+# def get_all_riddles():
+#     try:
+#         riddles = list(riddles_collection.find())
+#         return jsonify(riddles)
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=8000)
+
+
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 import random
@@ -5,7 +46,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-client = MongoClient("mongodb://root:root@localhost:27017")
+client = MongoClient("mongodb://root:root@34.87.85.100:27017")
 db = client["riddles"]
 riddles_collection = db["riddles_collection"]
 
@@ -14,6 +55,7 @@ def get_riddle():
     pipeline = [{'$sample': {'size': 1}}]
     try:
         random_riddle = riddles_collection.aggregate(pipeline).next()
+        random_riddle['_id'] = str(random_riddle['_id'])
         return jsonify(random_riddle)
     except StopIteration:
         return jsonify({"msg": "No riddles available"})
@@ -31,6 +73,8 @@ def add_riddle():
 def get_all_riddles():
     try:
         riddles = list(riddles_collection.find())
+        for item in riddles:
+            item['_id'] = str(item['_id'])
         return jsonify(riddles)
     except Exception as e:
         return jsonify({"error": str(e)})
