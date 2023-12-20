4a3a3e16eb0d3b379f8e84a87fa0a55054957b1d
Ankit <ankit.raut@coditas.com>
2023-12-20 23:21:30 +0530

    db fixed

--- Backend/main.py
+++ Backend/main.py
@@ -1,71 +1,38 @@
-# from flask import Flask, jsonify
-# from flask_cors import CORS
-# from pymongo import MongoClient
-
-# app = Flask(__name__)
-# CORS(app)
-
-# # for manual container deployement tyr this 
-# # client = MongoClient("mongodb://localhost:27017")
-
-# client = MongoClient("mongodb://root:root@127.0.0.1:27017")
-
-
-# db = client["riddles"]
-# collection = db["riddles_collection"]
-
-# @app.route('/get_data', methods=['GET'])
-# def get_data():
-#     # Fetch data from MongoDB
-#     cursor = collection.find({})
-#     data = list(cursor)
-
-#     # Convert ObjectId to str for JSON serialization
-#     for item in data:
-#         item['_id'] = str(item['_id'])
-#     print(data)
-#     return jsonify(data)
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", debug=True, port=8000)
-
-import flask_cors
 from flask import Flask, jsonify, request
-import pymongo
 from pymongo import MongoClient
 import random
 
 app = Flask(__name__)
-flask_cors.CORS(app)
 
-# Connect to MongoDB
-# client = MongoClient("mongodb://localhost:27017")
-client = MongoClient("mongodb://root:root@127.0.0.1:27017")
-db = client["your_database_name"]
-collection = db["your_collection_name"]
+client = MongoClient("mongodb://root:root@localhost:27017")
+db = client["riddles"]
+riddles_collection = db["riddles_collection"]
 
-@app.route('/get_riddle', methods=['GET'])
+@app.route('/get_riddle')
 def get_riddle():
-    # Retrieve a random riddle from MongoDB
-    random_riddle = collection.aggregate([{ '$sample': {'size': 1} }]).next()
-
-    # Return the riddle as JSON
-    return jsonify({"riddle": random_riddle["riddle"], "ans": random_riddle["ans"]})
+    pipeline = [{'$sample': {'size': 1}}]
+    try:
+        random_riddle = riddles_collection.aggregate(pipeline).next()
+        return jsonify(random_riddle)
+    except StopIteration:
+        return jsonify({"msg": "No riddles available"})
 
 @app.route('/add_riddle', methods=['POST'])
 def add_riddle():
-    new_riddle = request.json
+    try:
+        new_riddle = request.json
+        riddles_collection.insert_one(new_riddle)
+        return jsonify({"msg": "Riddle added successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
-    # Add the new riddle to MongoDB
-    collection.insert_one(new_riddle)
-
-    return jsonify({"message": "Riddle added successfully"})
-
-@app.route('/get_all_riddles', methods=['GET'])
+@app.route('/get_all_riddles')
 def get_all_riddles():
-    all_riddles = list(collection.find({}, {'_id': 0}))
-    return jsonify({"riddles": all_riddles})
+    try:
+        riddles = list(riddles_collection.find())
+        return jsonify(riddles)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+    app.run(debug=True, host='0.0.0.0', port=8000)
