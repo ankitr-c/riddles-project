8cd150425d595d05f6f50a6637a93aaa4dd42a64
Ankit <ankit.raut@coditas.com>
2023-12-26 10:58:10 +0530

    added pgsql code

---
+++ Backend/ndb_main.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, request
+import psycopg2
+from flask_cors import CORS
+# import random
+
+app = Flask(__name__)
+CORS(app)
+
+# Replace the connection parameters with your PostgreSQL database details
+conn = psycopg2.connect(
+    dbname="riddles",
+    user="postgres",
+    password="root",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+@app.route('/get_riddle')
+def get_riddle():
+    try:
+        cursor.execute("SELECT riddle, ans FROM riddles_table ORDER BY RANDOM() LIMIT 1;")
+        result = cursor.fetchone()
+
+        if result:
+            return jsonify({"riddle": result[0], "ans": result[1]})
+        else:
+            return jsonify({"msg": "No riddles available"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    finally:
+        conn.commit()
+
+@app.route('/add_riddle', methods=['POST'])
+def add_riddle():
+    try:
+        new_riddle_data = request.json
+        cursor.execute("INSERT INTO riddles_table (riddle, ans) VALUES (%s, %s);",
+                       (new_riddle_data.get('riddle'), new_riddle_data.get('ans')))
+        return jsonify({"msg": "Riddle added successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    finally:
+        conn.commit()
+
+@app.route('/get_all_riddles')
+def get_all_riddles():
+    try:
+        cursor.execute("SELECT riddle, ans FROM riddles_table;")
+        results = cursor.fetchall()
+        riddles_data = [{"riddle": result[0], "ans": result[1]} for result in results]
+        return jsonify(riddles_data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    finally:
+        conn.commit()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
